22efa7f7f3e792f67cb88a65019d5989181bb3f3
ruhulkuddus123 <ruhul15-14171@diu.edu.bd>
2021-10-02 23:21:37 +0600

    points on line some changes

--- Devskill/Intermediate/DevCP - Intermediate - B11 - Binary Search/B - Points on Line.cpp
+++ Devskill/Intermediate/DevCP - Intermediate - B11 - Binary Search/B - Points on Line.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define MAX 100000
 
 int main()
 {
     int n, d;
     scanf("%d%d", &n, &d);
     vector<int> vc(n);
-    for(int &e : vc) //take input val
-        cin>> e;
+    for(int &e : vc) //take input
+        scanf("%d", &e);
     ll ans = 0;
     for(int i = 0; i < n; i++)
     {
@@ -18,10 +17,8 @@ int main()
         ll up_bound = upper_bound(vc.begin(), vc.end(), limit) - vc.begin();
         ll points = up_bound - idx;
         if(points < 2) continue;
-        ll pC =points*(points - 1);
-        ans += pC/2;
+        ans += (points * (points - 1))/2;
     }
-    printf("%d\n", ans);
+    printf("%lld\n", ans);
     return 0;
 }
-
